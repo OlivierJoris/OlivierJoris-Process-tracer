382d5ec06e8a687d1f0b2d61cd89a6c3490eb1cb
Maxime Goffart <maximegoffart@icloud.com>
2021-03-05 14:53:34 +0100

    [ARGS PARSE] modified return type

--- test_arguments.c
+++ test_arguments.c
@@ -1,10 +1,26 @@
-#include <stdlib.h>
-
 #include "util.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
-    int ret = load_arguments(argc, argv);
+    LaunchMode lm = load_arguments(argc, argv);
+    switch (lm)
+    {
+        case profiler:
+            printf("Detected profiler mode\n");
+            break;
+        case syscall:
+            printf("Detected syscall mode\n");
+            break;
+        case error:
+            fprintf(stderr, "Error while parsing arguments!\n");
+            return -1;
+    
+        default:
+            fprintf(stderr, "Invalid arguments!\n");
+            return -1;
+    }
 
-    return ret;
+    return EXIT_SUCCESS;
 }
--- util.c
+++ util.c
@@ -1,44 +1,37 @@
+/*
+ * Module implementing the utility interface.
+ * 
+ * @author Maxime Goffart (180521) & Olivier Joris (182113)
+ */
+
+#include "util.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "util.h"
-
-/* 
- * Represents the launch mode of the program..
- */
-typedef enum
-{
-    profiler,
-    syscall
-}LaunchMode;
-
-int load_arguments(int argc, char **argv)
+LaunchMode load_arguments(int argc, char **argv)
 {
     if(argc != 3)
     {
         fprintf(stderr, "Please launch this program with ./tracer <mode> <prog>\n");
-        return EXIT_FAILURE;
+        return error;
     }
-
-    LaunchMode mode;
 
     if(!strcmp(argv[1], "-p"))
     {
-        mode = profiler;
         printf("Program launched in profiler mode on the %s tracee\n.", argv[2]);
-        return EXIT_SUCCESS;
+        return profiler;
     }
     else
     {
-        if(!strcmp(argv[1], "-m"))
+        if(!strcmp(argv[1], "-s"))
         {
-            mode = syscall;
             printf("Program launched in syscall mode on the %s tracee\n.", argv[2]);
-            return EXIT_SUCCESS;
+            return syscall;
         }
         else 
             fprintf(stderr, "Please choose among the proposed modes (profiler or syscall mode).\n");
-            return EXIT_FAILURE;
+            return error;
     }
 }
