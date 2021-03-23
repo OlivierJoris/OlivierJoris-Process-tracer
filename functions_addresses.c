e18f1be0d89af474fd21ae7d9f2c9257026d9724
Maxime Goffart <maximegoffart@icloud.com>
2021-03-23 13:04:03 +0100

    Update functions_addresses

--- functions_addresses.c
+++ functions_addresses.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Remembers if first call to function_address_get_symbol_deref();
 static char first_call = 1;
 
 /*
@@ -28,10 +29,10 @@ struct FunctionsAddresses_t {
 };
 
 /*
- * Generate the command (to retreive the mapping) to be
+ * Generates the command (to retreive the mapping) to be
  * executed based on the name of the executable.
  * 
- * @param exec: path to the executable for which we want the
+ * @param exec Path to the executable for which we want the
  * mapping.
  * 
  * @return The command to be executed.
@@ -39,10 +40,10 @@ struct FunctionsAddresses_t {
 static char* generate_command_nm(char* exec);
 
 /*
- * Generate the command (to retreive the mapping) between
+ * Generates the command (to retreive the mapping) between
  * functions' addresses and symbols.
  * 
- * @param exec: path to the executable for which we want the mapping.
+ * @param exec Path to the executable for which we want the mapping.
  * 
  * @return The command to be executed.
  */
@@ -64,19 +65,24 @@ FunctionsAddresses* functions_addresses_load(char* exec){
         return NULL;
     }
 
-    const int BUFFER_SIZE = 128;
+    static const int BUFFER_SIZE = 128; // Size of a string
 
     FunctionsAddresses* fa = malloc(sizeof(FunctionsAddresses));
-    if(!fa)
+    if(!fa){
+        fprintf(stderr, "Unable to allocate memory to fetch the mapping!\n");
         return NULL;
+    }
+
     fa->first = create_new_cell(0, BUFFER_SIZE);
     if(!fa->first){
+        fprintf(stderr, "Unable to allocate memory to fetch the mapping!\n");
         functions_addresses_clean(fa);
         return NULL;
     }
 
     char* command = generate_command_nm(exec);
     if(!command){
+        fprintf(stderr, "Unable to generate the command to fetch the mapping!\n");
         functions_addresses_clean(fa);
         return NULL;
     }
@@ -85,6 +91,7 @@ FunctionsAddresses* functions_addresses_load(char* exec){
 
     FILE* f = fopen("nm.txt", "r");
     if(!f){
+        fprintf(stderr, "Unable to open file to fetch the mapping!\n");
         functions_addresses_clean(fa);
         return NULL;
     }
@@ -93,11 +100,14 @@ FunctionsAddresses* functions_addresses_load(char* exec){
     char buffer[BUFFER_SIZE];
     Mapping* current = fa->first;
     while(fscanf(f, "%lx %s\n", &address, buffer) != -1){
+        // Saves current line in the file
         current->addr = address;
         strcpy(current->symbol, buffer);
 
+        // Builds new line in the file
         current->next = create_new_cell(0, BUFFER_SIZE);
         if(!current->next){
+            fprintf(stderr, "Unable to allocate memory to fetch the mapping!\n");
             functions_addresses_clean(fa);
             fclose(f);
             return NULL;
@@ -111,42 +121,57 @@ FunctionsAddresses* functions_addresses_load(char* exec){
     current = fa->first;
     Mapping* prev = NULL;
     char* command2 = generate_command_objdump(exec);
-    if(!command2)
-        return fa;
+    if(!command2){
+        fprintf(stderr, "Unable to generate the command to fetch the mapping!\n");
+        functions_addresses_clean(fa);
+        return NULL;
+    }
 
     system(command2);
     free(command2);
 
     FILE* f2 = fopen("func_names.txt", "r");
-    if(!f2)
-        return fa;
+    if(!f2){
+        fprintf(stderr, "Unable to open file to fetch the mapping!\n");
+        functions_addresses_clean(fa);
+        return NULL;
+    }
 
     while(fscanf(f2, "%lx %s\n", &address, buffer) != -1){
         current = fa->first;
         prev = NULL;
         while(current != NULL){
-            if(current->addr == address) // Already have the mapping in memory
+            // Already have the mapping in memory
+            if(current->addr == address)
                 break;
+
             // New cell at the beginning of the list
             if(prev == NULL && address < current->addr){
                 Mapping* newCell = create_new_cell(address, BUFFER_SIZE);
                 if(!newCell){
+                    fprintf(stderr, "Unable to allocate memory to fetch the mapping!\n");
+                    functions_addresses_clean(fa);
                     fclose(f2);
-                    return fa;
+                    return NULL;
                 }
+
                 strcpy(newCell->symbol, buffer);
                 Mapping* tmp = fa->first;
                 fa->first = newCell;
                 newCell->next = tmp;
                 break;
             }
+
             // New cell in the middle of the list
             if(prev != NULL && address > prev->addr && address < current->addr){
                 Mapping* newCell = create_new_cell(address, BUFFER_SIZE);
                 if(!newCell){
+                    fprintf(stderr, "Unable to allocate memory to fetch the mapping!\n");
+                    functions_addresses_clean(fa);
                     fclose(f2);
                     return fa;
                 }
+
                 strcpy(newCell->symbol, buffer);
                 newCell->next = prev->next;
                 prev->next = newCell;
@@ -167,11 +192,13 @@ static Mapping* create_new_cell(unsigned long address, unsigned long symbolLengt
     Mapping* newCell = malloc(sizeof(Mapping));
     if(!newCell)
         return NULL;
+
     newCell->symbol = calloc(symbolLength, sizeof(char));
     if(!newCell->symbol){
         free(newCell);
         return NULL;
     }
+
     newCell->addr = address;
     return newCell;
 }
@@ -245,7 +272,7 @@ char* function_address_get_symbol_deref(char* tracee, unsigned long addr){
         first_call = 0;
     }
 
-    // Find intermediate address
+    // Finds intermediate address
     sprintf(buffer, "cat dump_s.txt | grep -oE \".*%lx.*ff 15.*call.*\" | awk '{print $NF}' | grep -oE \"8.*\" > going_addr.txt", addr);
     system(buffer);
 
@@ -256,7 +283,7 @@ char* function_address_get_symbol_deref(char* tracee, unsigned long addr){
     fscanf(f, "%lx", &goingAddr);
     fclose(f);
 
-    // Find dest address
+    // Finds dest address
     sprintf(buffer, "cat dump_s.txt | grep -oE \".*%lx [0-9a-z]{8}[ ]{1}\" | awk '{print $2}' > going_addr.txt", goingAddr);
     system(buffer);
 
@@ -268,8 +295,10 @@ char* function_address_get_symbol_deref(char* tracee, unsigned long addr){
     fclose(f);
 
     char* tmp = malloc(sizeof(char) * (strlen(reachedAddr) + 1));
-    if(!tmp)
+    if(!tmp){
+        fprintf(stderr, "Unable to allocate memory to fetch function's symbol!\n");
         return NULL;
+    }
     unsigned int i = 0;
     for(; i < strlen(reachedAddr); ++i){
         if(i%2){
@@ -280,7 +309,7 @@ char* function_address_get_symbol_deref(char* tracee, unsigned long addr){
     }
     tmp[i] = '\0';
 
-    // Find name of function
+    // Finds name of function
     sprintf(buffer, "cat dump_s.txt | grep -oE \".*%s.*<.*>\" | awk '{print $2}' | grep -oE \"[^<]{1}.*[^>]{1}\" > func_name.txt", tmp);
     system(buffer);
     free(tmp);
@@ -299,20 +328,4 @@ char* function_address_get_symbol_deref(char* tracee, unsigned long addr){
     fclose(f);
     
     return funcName;
-}
-
-unsigned long function_addresses_get_addr(FunctionsAddresses* fa, char* symbol){
-    if(!fa || !fa->first)
-        return 0;
-
-    unsigned long addr = 0;
-
-    Mapping* mapping = fa->first;
-    while(mapping!= NULL){
-        if(!strcmp(symbol, mapping->symbol))
-            return mapping->addr;
-        mapping = mapping->next;
-    }
-
-    return addr;
 }
--- functions_addresses.h
+++ functions_addresses.h
@@ -1,6 +1,8 @@
 /*
- * Interface to get mapping between function symbols and
- * addresses. Requires nm command to be available.
+ * Interface to get mapping between functions' symbols and
+ * addresses.
+ * 
+ * @warning Requires nm & objdump to be available.
  * 
  * @author Maxime Goffart (180521) & Olivier Joris (182113)
  */
@@ -16,9 +18,10 @@ typedef struct FunctionsAddresses_t FunctionsAddresses;
 
 /*
  * Loads mapping between functions' addresses and symbols.
- * Need to call functions_addresses_clean() after usage.
  * 
- * @param exec: path to the executable for which we want the
+ * @warning Needs to call functions_addresses_clean() after usage.
+ * 
+ * @param exec Path to the executable for which we want the
  * mapping.
  * 
  * @return Pointer to the mapping.
@@ -28,15 +31,15 @@ FunctionsAddresses* functions_addresses_load(char* exec);
 /*
  * Cleans the mapping.
  * 
- * @param fa: pointer to the mapping.
+ * @param fa Pointer to the mapping.
  */
 void functions_addresses_clean(FunctionsAddresses* fa);
 
 /*
  * Gets the symbol associated to the given address.
  * 
- * @param fa: pointer to the mapping.
- * @param addr: address for which we want the associated symbol.
+ * @param fa Pointer to the mapping.
+ * @param addr Address for which we want the associated symbol.
  * 
  * @return Symbol associated to the given address.
  */
@@ -48,18 +51,8 @@ char* functions_addresses_get_symbol(FunctionsAddresses* fa, unsigned long addr)
  * @param tracee Path to the executable of the tracee.
  * @param addr Address of instruction when the call occured.
  * 
- * @return Name of called function.
+ * @return Symbol of called function.
  */
 char* function_address_get_symbol_deref(char* tracee, unsigned long addr);
-
-/*
- * Gets the address associated to the given symbol.
- * 
- * @param fa: pointer to the mapping.
- * @param symbol: symbol for which we want the associated address.
- * 
- * @return Address associated to the given symbol.
- */
-unsigned long function_addresses_get_addr(FunctionsAddresses* fa, char* symbol);
 
 #endif
